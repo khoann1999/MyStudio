14f2b2782bddf5923f2f6dfcb15ea92042f8e82b
khoaNN <khoann1999@gmail.com>
2020-12-18 14:53:12 +0700

    fix login

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -44,7 +44,7 @@ namespace MyStudioApp.Controllers
         [HttpPost("/api/Accounts/login")]
         public async Task<ActionResult<Account>> LoginAccount([FromBody] Account account)
         {
-            var result = await _context.Accounts.FirstAsync(user => user.Username.Equals(account.Username) && user.Password.Equals(account.Password));
+            var result = await _context.Accounts.Include(actor => actor.Actor).FirstAsync(user => user.Username.Equals(account.Username) && user.Password.Equals(account.Password));
 
             if (result == null)
             {
@@ -53,6 +53,7 @@ namespace MyStudioApp.Controllers
 
             return result;
         }
+
         // PUT: api/Accounts/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
